fb86d065b88d67e4915d9edb98d2496f4444c7a2
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-07-25 01:47:57 +0300

    task_1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+
+
+// Task 1
+
+let a='true';
+let b= false;
+let c= 17;
+let d= undefined;
+let f=null;
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof f);
